ded6484f579544f600ef96f5efe17ca70f2963e6
Caio Oliveira <caioinacio00@gmail.com>
2023-01-10 20:58:21 -0300

    toDo

---
+++ toDo.mk
@@ -0,0 +1,9 @@
+- Caio:
+	- limpar codigo e ajustar o código
+	- ajustar dropdown 'sobre nos'
+	- ajustar button 'responsivo'
+	- fazer imagens das paginas no canva
+	- desenvolver footer (bootstrap)
+
+- Jonas:
+	- pesquisar ideias para o desenvolvimento das páginas
