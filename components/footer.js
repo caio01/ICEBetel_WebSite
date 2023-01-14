5f395841facefaea8acb9a247d71d020da54292a
Caio Oliveira <caioinacio00@gmail.com>
2023-01-14 12:33:59 -0300

    add comentarios footer

--- components/footer.js
+++ components/footer.js
@@ -1,25 +1,32 @@
 document.getElementById('footer').innerHTML = 
 `
+<!-- div com as configuracoes do footer -->
 <footer class="bg-dark text-center text-white">
   <div class="container p-4 pb-0">
     <section class="mb-4">
+
+      <!-- icone Youtube -->
       <a class="btn text-white btn-floating m-1" style="background-color: #f44336;" 
         href="https://www.youtube.com/@canalicebetel" role="button">
         <i class="fab fa-youtube"></i>
       </a>
 
+      <!-- icone Facebook -->
       <a class="btn text-white btn-floating m-1" style="background-color: #3b5998;"
         href="https://www.facebook.com/icebetel" role="button">
         <i class="fab fa-facebook-f"></i>
       </a>
 
+      <!-- icone Instagram -->
       <a class="btn text-white btn-floating m-1" style="background-color: #ac2bac;" 
         href="#!" role="button">
         <i class="fab fa-instagram"></i>
       </a>
+
     </section>
   </div>
 
+  <!-- copyrigth -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 
     <a class="text-white">Igreja Cristã Evangélica Betel</a>
