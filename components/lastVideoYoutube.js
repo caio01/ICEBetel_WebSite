64684b30c867e6ed156fdbc7b68a15164a75cd1f
Caio Oliveira <caioinacio00@gmail.com>
2023-01-26 12:25:14 -0300

    integrando com a API do YouTube

--- components/lastVideoYoutube.js
+++ components/lastVideoYoutube.js
@@ -1,19 +1,41 @@
-const apiKey = 'AIzaSyD19NhX4SXowIe2VvpnlzzJX16gnRI_4aw'
-const channelID = 'UCocY6WNm4TSu4aUlWdyoGtw'
-const maxResults = 999
-const url = 'https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&channelId=' + channelID + 
-            '&part=snippet,id&order=date&maxResults=' + (maxResults > 1 ? 1 : maxResults)
+// funcao assincrona que recupera o ultimo video publicado no canal
+async function getApiYoutube() {
+    // chave da api
+    const apiKey = 'AIzaSyD19NhX4SXowIe2VvpnlzzJX16gnRI_4aw'
+    // id do canal
+    const channelID = 'UCocY6WNm4TSu4aUlWdyoGtw'
+    // numero maximo de resultados que a api retornara
+    const maxResults = 1
+    // url base para a consulta
+    const url = 'https://www.googleapis.com/youtube/v3/search?key=' + apiKey + '&channelId=' + channelID + 
+                '&part=snippet,id&order=date&maxResults=' + maxResults
 
+    // get utilizando o fetch API
+    await fetch(url)
+        // convertendo a resposta para um objeto JSON
+        .then(T => T.json())
+        // funcao que trata a resposta já convertida
+        .then(response => {
 
+            // recupera o id do video
+            videoID = response.items[0].id.videoId
+            // define a classe do iframe
+            iClass = "embed-responsive-item"
+            // define o src do iframe que aponta para o video
+            iSrc = "https://www.youtube.com/embed/" + videoID
 
-fetch(url)
-    .then(T => T.json())
-    .then(response => {
-        videoID = response.items[0].id.videoId
-        htmlString = '<iframe class="embed-responsive-item" src="' + "https://www.youtube.com/embed/" + videoID + 
-                        '" frameborder="0" allowfullscreen></iframe>'
-        document.createElement()
-        document.body.getElementsByClassName('embed-responsive').innerHTML = htmlString    
-    })
+            // cria o elemento iframe
+            iframe = document.createElement('iframe')
+            // seta a classe do iframe
+            iframe.className = iClass
+            // seta o src do iframe
+            iframe.src = iSrc
 
-    
+            // aninha o iframe na div de ID: 'youtubeVideo'
+            document.querySelector("#youtubeVideo").appendChild(iframe)
+
+        })
+}
+
+// chama a funcao
+getApiYoutube()
